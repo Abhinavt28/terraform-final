bb4082e9947d404c02d7f75dc5cad57a42933cbc
hardbro786 <126283577+hardbro786@users.noreply.github.com>
2025-11-18 12:37:28 +0530

    Create variables.tf

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,10 @@
+variable "ami_id" {}
+variable "instance_type" {}
+variable "key_name" {}
+
+variable "public_subnet" {}
+variable "private_subnet1" {}
+variable "private_subnet2" {}
+
+variable "public_sg_id" {}
+variable "private_sg_id" {}
